51bf4bb86584b1672e8088496c5c314fbe3508e4
azazello15 <102389631+azazello15@users.noreply.github.com>
2023-08-31 14:42:04 +0300

    Add files via upload

---
+++ bot/src/bot_app/app.py
@@ -0,0 +1,9 @@
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+from .local_settings import API_KEY
+
+bot = Bot(token=API_KEY)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
---
+++ bot/src/bot_app/messages.py
@@ -0,0 +1,6 @@
+WELCOME_MESSAGE = '''
+Hello! I can help you to memorize German\'s nouns gender!
+Comands are: \n
+/train_ten\n
+/train_all\n
+'''
